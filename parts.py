c08f296f01590f0c80624a1ae87fbcf2c081fff2
alwinglee <alwin.g.lee@gmail.com>
2025-03-29 14:37:21 -0400

    Added Python documentation for the Parts class.

--- parts.py
+++ parts.py
@@ -1,9 +1,25 @@
 from turtle import Turtle
 
 class Parts(Turtle):
+    """
+    Represents the labels for each part of the C8 Carbine firearm.
+    Each instance is assigned its appropriate name and coordinates and is displayed on the screen.
+    The class also keeps track of all the successfully guessed parts to support the Reset feature.
+    """
+
+    # List to track all guessed C8 Carbine parts
     guessed_parts_list = []
 
     def __init__(self,part,x_coordinate,y_coordinate):
+        """
+        Creates an instance of the firearm part label after the user guesses it correctly.
+
+        :param part: The name of the successfully guessed firearm part.
+        :param x_coordinate: The horizontal position on the screen where the label will appear.
+        :param y_coordinate: The vertical position on the screen where the label will appear.
+
+        :return: None
+        """
         super().__init__()
         self.penup()
         self.hideturtle()
@@ -13,6 +29,11 @@ class Parts(Turtle):
 
     @classmethod
     def reset_parts(cls):
+        """
+        Remove all visible labels on the screen and empties the guessed parts list.
+
+        :return: None
+        """
         for part in cls.guessed_parts_list:
             part.clear()
         cls.guessed_parts_list = []
